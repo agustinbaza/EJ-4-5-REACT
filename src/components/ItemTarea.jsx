6c72b86e6f606b45c06f6a0a5be7d895a8646412
Agustin Baza <agustinbaza10@gmail.com>
2023-07-04 20:03:50 -0300

    Ejercicio pasado a limpio y terminado, correctamente funcionando

---
+++ src/components/ItemTarea.jsx
@@ -0,0 +1,16 @@
+import { Button, ListGroup } from "react-bootstrap";
+
+const ItemTarea = ({ tarea, borrarTarea }) => {
+  return (
+    <div>
+      <ListGroup.Item className="d-flex justify-content-between">
+        {tarea}
+        <Button variant="danger" onClick={() => borrarTarea(tarea)}>
+          Borrar
+        </Button>
+      </ListGroup.Item>
+    </div>
+  );
+};
+
+export default ItemTarea;
